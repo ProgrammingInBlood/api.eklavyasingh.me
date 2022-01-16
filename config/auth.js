7a1163a5c0bb4847cddaf1c1758862c11f586a1b
Eklavya Singh <eklavyasingh12065@gmail.com>
2022-01-16 20:37:32 +0530

    added passport

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
 const facebook = {
   clientID: process.env.FACEBOOK_CLIENT_ID,
   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-  callbackURL: `${BASE_URL}/auth/facebook/callback`,
+  callbackURL: `${BASE_URL}/api/auth/facebook/callback`,
   profileFields: ["id", "name", "displayName", "picture", "email"],
 };
 
